2052eba74d711fb083aeb378c3a9740580a01ae4
U201817044 <1219821955@qq.com>
2021-06-23 11:00:07 +0800

    用户注册部分实现

--- src/main/java/com/example/demo/service/LikeService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-public interface LikeService {
-    void insertLike(String userId,int musicId);
-    void deleteLikeByUserAndMusicId(String userId,int musicId);
-
-}
