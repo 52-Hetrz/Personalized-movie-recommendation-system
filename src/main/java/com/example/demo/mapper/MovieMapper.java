eaf9ad4a8762f628748612e8324bf4812112df18
U201817044 <1219821955@qq.com>
2021-06-24 08:51:03 +0800

    对电影名模糊查找功能进行了补充完善

--- src/main/java/com/example/demo/mapper/MovieMapper.java
+++ src/main/java/com/example/demo/mapper/MovieMapper.java
@@ -3,6 +3,7 @@ package com.example.demo.mapper;
 
 import com.example.demo.dao.Movie;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -13,5 +14,5 @@ public interface MovieMapper {
     ArrayList<Movie> selectCollectMoviesByUserId(int userId);
     String selectMovieNameById(int id);
     Movie selectMovieById(int id);
-    ArrayList<Movie> fuzzySearchMovieByName(String content);
+    ArrayList<Movie> fuzzySearchMovieByName(@Param("content") String content);
 }
